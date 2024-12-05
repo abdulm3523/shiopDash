0202ae2e8d9ed9659a610e941a9eb9ccbf418757
abdulm3523 <abdulmalik3523@gmail.com>
2024-12-06 01:34:22 +0300

    added pagination & completed the mvp projecct

--- shopApp/src/pages/productList.jsx
+++ shopApp/src/pages/productList.jsx
@@ -5,14 +5,18 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { LoadingIndigator } from "../component/LoadingIndigator";
 import { ProductCard } from "../component/productCard";
 import { ErrorHandel } from "../component/error";
+import { useState } from "react";
 
-const fetchProduct = async () => {
-  const response = await axios.get("http://localhost:3000/products");
+const fetchProduct = async ({ queryKey }) => {
+  const response = await axios.get(
+    `http://localhost:3000/products?_page=${queryKey[1].page}&_per_page=8`
+  );
 
   return response.data;
 };
 
 export const ProductList = () => {
+  const [page, setPage] = useState(1);
   console.log(fetchProduct());
 
   const {
@@ -20,7 +24,7 @@ export const ProductList = () => {
     error,
     isLoading,
   } = useQuery({
-    queryKey: ["products"],
+    queryKey: ["products", { page }],
     queryFn: fetchProduct,
   });
   const queryClient = useQueryClient();
@@ -44,9 +48,27 @@ export const ProductList = () => {
     <>
       <h1 className="text-center mb-4">Products</h1>
       <div className="row g-4">
-        {product.map((item) => (
+        {product.data.map((item) => (
           <ProductCard onDelete={handelOnDelete} product={item} key={item.id} />
         ))}
+      </div>
+      <div className="d-flex justify-content-center align-items-center">
+        {product.prev && (
+          <button
+            className="btn btn-primary mt-3"
+            onClick={() => setPage(product.prev)}
+          >
+            Prev
+          </button>
+        )}
+        {product.next && (
+          <button
+            className="btn btn-primary mt-3"
+            onClick={() => setPage(product.next)}
+          >
+            Next
+          </button>
+        )}
       </div>
     </>
   );
